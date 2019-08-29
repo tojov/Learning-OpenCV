b5843903198c27637b6b4a2b18f6c17fa992abdd
Tom Johnson V <tomv.johnson2100@gmail.com>
2019-08-29 08:04:36 +0530

    Update greyscale.py

--- greyscale.py
+++ greyscale.py
@@ -1,8 +1,7 @@
 import cv2
-
-image = cv2.imread("clouds.jpg")
-gray_image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Over the Clouds",image)
-cv2.imshow("Over the Clouds - gray" , gray_image)
+image = cv2.imread("image_name.jpg") #image_name is the name of the image that has to be converted to greyscale.
+grey_image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #grey_image is the name of the converted image. 
+cv2.imshow("image name",image)
+cv2.imshow("grey image" , grey_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
